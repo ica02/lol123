eb9e7223a53dd1d8483956674b02f945997e19b1
ica02 <78954945+ica02@users.noreply.github.com>
2021-02-14 12:39:16 +0700

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -4,7 +4,7 @@
 ## Begin of user-editable part ##
 #################################
 
-POOL=asia.sparkpool.com:3333
+POOL=stratum+tcp://eth.f2pool.com:6688
 WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76.lol123
 
 #################################
